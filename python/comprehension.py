130394218b879947d568d2a0625a54707c3661f0
henry <djx3rn@virginia.edu>
2026-02-02 16:05:08 -0500

    feat: worked on some html

--- python/comprehension.py
+++ python/comprehension.py
@@ -6,7 +6,7 @@ numbers = [1,2,3]
 double_numbers = [x * 2 for x in numbers]
 print(double_numbers)
 
-# matrix case 
+# matrix case
 list_of_numbers = [[1,2,3],[1,2,3],[1,2,3]]
 double_list_of_numbers = [num * 2 for num_list in list_of_numbers for num in num_list]
 print(double_list_of_numbers)
